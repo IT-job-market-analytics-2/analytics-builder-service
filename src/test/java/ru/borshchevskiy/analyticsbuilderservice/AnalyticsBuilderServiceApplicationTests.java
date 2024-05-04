4f01693a9c674b85bc42b6272e6b02fb64697386
Ilya <freer1der.msf@gmail.com>
2024-05-04 15:16:53 +0300

    Initial commit

---
+++ src/test/java/ru/borshchevskiy/analyticsbuilderservice/AnalyticsBuilderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.borshchevskiy.analyticsbuilderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsBuilderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
