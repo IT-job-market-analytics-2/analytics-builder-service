91a1dd2e0d2db8dc7bdcedcb235303c0a4ef7376
Ilya <freer1der.msf@gmail.com>
2024-05-13 14:40:55 +0300

    VacancyAnalyticsService - added JavaDoc, minor fix in updateAnalyticsData method

--- src/main/java/ru/borshchevskiy/analyticsbuilderservice/service/VacancyAnalyticsService.java
+++ src/main/java/ru/borshchevskiy/analyticsbuilderservice/service/VacancyAnalyticsService.java
@@ -32,6 +32,19 @@ public class VacancyAnalyticsService {
         this.vacancyAnalyticsMapper = vacancyAnalyticsMapper;
     }
 
+    /**
+     * Method builds analytics data per each query that it finds among all vacancies.
+     * <p>
+     * First, method acquires all vacancies with non-null salary and required currency (RUR).
+     * Then, iterating over these vacancies, for every query it finds,
+     * it accumulates analytical data into {@link VacancyAnalyticsDto}.
+     * <p>
+     * As a result of these operations a <code>perQueryVacancyAnalyticsMap</code> is created,
+     * mapping query to {@link VacancyAnalyticsDto} which holds analytical data on all vacancies related to that query.
+     * <p>
+     * Finally, method calls {@link VacancyAnalyticsService#saveAnalytics(String, VacancyAnalyticsDto)} for each
+     * <code>perQueryVacancyAnalyticsMap</code> entry.
+     */
     public void buildAnalytics() {
         log.debug("Started building analytics");
         List<VacancyEntity> vacancies = vacancyService.findAllWithSalaryCurrency(Currency.RUR);
@@ -50,6 +63,18 @@ public class VacancyAnalyticsService {
         }
     }
 
+    /**
+     * Method attempts to create {@link VacancyAnalytics} object from
+     * {@link VacancyAnalyticsDto} and save it to database.
+     * <p>
+     * First, method checks if analytics data for specified <code>query</code> and <code>createdAt</code> date
+     * is already present in database. If present, it updates existing data with new values.
+     * If there is no existing data, it creates new entity
+     * using {@link VacancyAnalyticsMapper#mapToEntity(VacancyAnalyticsDto, String, LocalDate)} and saves it to
+     * database.
+     * @param query query for which analytics data should be stored.
+     * @param vacancyAnalyticsDto object, holding analytics data.
+     */
     public void saveAnalytics(String query, VacancyAnalyticsDto vacancyAnalyticsDto) {
         LocalDate createdAt = LocalDate.now();
         log.debug("Starting saving analytics data for query: {}, on date: {}.", query, createdAt);
@@ -74,12 +99,26 @@ public class VacancyAnalyticsService {
         );
     }
 
+    /**
+     * Method creates analytics data for single vacancy,
+     * specifying vacancy count as 1 and calculating average salary for this vacancy.
+     * @param vacancy VacancyEntity on which analytics data is created.
+     * @return VacancyAnalyticsDto containing analytics data on single vacancy.
+     */
     private VacancyAnalyticsDto getSingleVacancyAnalytics(VacancyEntity vacancy) {
         return new VacancyAnalyticsDto(1, calculateVacancySalary(vacancy.getSalaryEntity()));
     }
 
+    /**
+     * Method merges <code>newData</code>, which is most commonly analytics data for single vacancy,
+     * with <code>existingData</code> existing analytics data. Updates original <code>existingData</code> object.
+     * Average salary is calculated as cumulative moving average.
+     * @param existingData existing analyticsData.
+     * @param newData new data to be merged to existing.
+     * @return updated <code>existingData</code>.
+     */
     private VacancyAnalyticsDto updateAnalyticsData(VacancyAnalyticsDto existingData, VacancyAnalyticsDto newData) {
-        int newVacanciesCount = existingData.getVacancyCount() + 1;
+        int newVacanciesCount = existingData.getVacancyCount() + newData.getVacancyCount();
         double avgSalary = existingData.getAverageSalary();
         double salary = newData.getAverageSalary();
         double newAvgSalary = avgSalary + (salary - avgSalary) / newVacanciesCount;
@@ -88,6 +127,13 @@ public class VacancyAnalyticsService {
         return existingData;
     }
 
+    /**
+     * Method calculates average salary for single vacancy.
+     * If both upper and lower salary limits are specified - calculates average value.
+     * If either upper or lower limit is specified - just takes this value as salary.
+     * @param salary Salary entity specified in vacancy.
+     * @return Double value of average salary calculated.
+     */
     private Double calculateVacancySalary(SalaryEntity salary) {
         if (salary.getFrom() != null && salary.getTo() != null) {
             return (double) ((salary.getFrom() + salary.getTo()) / 2);
