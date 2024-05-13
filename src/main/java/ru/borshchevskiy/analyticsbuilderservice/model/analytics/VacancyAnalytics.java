4a5a510504ce89a85a40c9f27a63e7e690e0d6c5
Ilya <freer1der.msf@gmail.com>
2024-05-13 12:50:34 +0300

    Added @Id annotation in VacancyAnalytics

--- src/main/java/ru/borshchevskiy/analyticsbuilderservice/model/analytics/VacancyAnalytics.java
+++ src/main/java/ru/borshchevskiy/analyticsbuilderservice/model/analytics/VacancyAnalytics.java
@@ -1,6 +1,7 @@
 package ru.borshchevskiy.analyticsbuilderservice.model.analytics;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 @Data
 @Table("Vacancy_analytics")
 public class VacancyAnalytics {
+    @Id
     private Long id;
     private LocalDate createdAt;
     private String query;
